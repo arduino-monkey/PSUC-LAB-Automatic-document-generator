ade76380fd7a7e52f4b97f9ca22a5714a8ce4f17
Arya Anand <67004663+arduino-monkey@users.noreply.github.com>
2021-08-28 16:21:52 +0530

    lab4 ques3

--- answers/answers4/3.c
+++ answers/answers4/3.c
@@ -1,9 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 
-int main(){
+int factorial(int n){
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main()
+{
     //add details
-    int n;
-    printf("enter n: ");
-    scanf("%d", &n);
     
+    int degrees, n, sign = 1;
+    printf("Enter n and angle(in degrees): ");
+    scanf("%d %d", &n, &degrees);
+    
+    float radians = degrees * (3.14159 / 180.0);
+    float sum = 0;
+
+    for (int power = 1; power <= n * 2; power += 2)
+    {
+        sum += sign * (pow(radians, power) / factorial(power));
+        sign *= -1;
+    }
+    printf("Sin(%d) = %f", degrees, sum);
+    return 0;
 }
